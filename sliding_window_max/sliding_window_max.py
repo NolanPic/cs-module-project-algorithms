99a625baba7589905fd72cd230f73dafbf9286f2
Nolan Picini <nolanpic@gmail.com>
2020-05-20 15:41:33 -0700

    complete sliding_window_max.py

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,35 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # start by looping through nums (i for nums)
+    # next, loop from i to i+k to get window
+    # next, find the largest item in that range
+    # append to an array
+    max_nums = []
+    for i in range(len(nums)):
+        # set the maximum number to negative infinity
+        # so that it will always be overwritten, even
+        # if there are negative numbers in the array
+        max_num = float("-inf")
+        for j in range(i, i+k):
+            # ensure we are not at the end of the array
+            if j < len(nums):
+                # find the max num in this set
+                if nums[j] > max_num:
+                    max_num = nums[j]
+            else:
+                # we are at the end of the array
+                break
+        # max_num will now be the largest item. Append
+        max_nums.append(max_num)
+        if j == len(nums)-1: 
+            # while the outer loop (i) has not finished,
+            # j has successfully counted up to the end of
+            # the array, and therefore we need to break out
+            # of the i loop
+            break  
 
-    pass
+    return max_nums
 
 
 if __name__ == '__main__':
