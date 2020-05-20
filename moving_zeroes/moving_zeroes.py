1e755c66854e9d81c838957e551927cf2f309e4d
Nolan Picini <nolanpic@gmail.com>
2020-05-20 12:43:00 -0700

    completed single_number.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,25 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
+    # loop thru the array, and while the value on the current
+    # value's RIGHT is zero, swap
+    for i in range(len(arr)):
+        # make sure we are not at the start of the array--
+        # can't swap at this point
+        if i is not 0:
+            # while the previous item is zero, swap
+            prev = arr[i-1]
+            while prev is 0 and arr[i] is not 0:
+                arr[i], arr[i-1] = arr[i-1], arr[i]
+    return arr
+            
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+    # [0, 3, 1, 0, -2]
+    # [3, 1, 0, -2, 0]
 
+    print('is it an infinate loop?')
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    print("No, you're all good!")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,21 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    # outer loop
+    for i in range(len(arr)):
+        # declare variable for if a number is found twice
+        found_twice = False
+        # loop thru array for every item in array
+        for j in range(len(arr)):
+            # if we aren't looking at the same index (same item)
+            # and the value is the same, the item was found
+            if i != j and arr[i] == arr[j]:
+                found_twice = True
+        # if the value was not found twice, return that value
+        if not found_twice:
+            return arr[i]
+    
+    return -1
 
 
 if __name__ == '__main__':
