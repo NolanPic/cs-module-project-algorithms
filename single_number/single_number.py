ddf2caa6ac9c43b9f362ddad8310c61f14d296ad
Nolan Picini <nolanpic@gmail.com>
2020-05-21 12:30:50 -0700

    improved on single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,7 +2,8 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
+# O(n^2)
+def single_number_array(arr):
     # outer loop
     for i in range(len(arr)):
         # declare variable for if a number is found twice
@@ -18,6 +19,28 @@ def single_number(arr):
             return arr[i]
     
     return -1
+
+
+# O(n)
+def single_number(nums):
+    # store each single number found (there should only be one)
+    single_counts = {}
+    
+    for x in nums: # O(n)
+        if x in single_counts: # O(1)
+            # if x is already in single_counts, that means
+            # it is not actually a single num--thus delete it
+            del single_counts[x]
+        else:
+            # this num has only been found once so far
+            single_counts[x] = 1
+            
+    # single_counts will only contain one key, but we still have
+    # to loop through it to get to it. The loop will be O(1)
+    # because there will always only ever be 1 key
+    for number in single_counts:
+        return number
+        
 
 
 if __name__ == '__main__':
