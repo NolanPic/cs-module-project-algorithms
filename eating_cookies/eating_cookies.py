118b4a5e4f79a3888edc2792a303eac2bb3df7b1
Nolan Picini <nolanpic@gmail.com>
2020-05-21 12:17:43 -0700

    Day2: improve eating_cookies runtime

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,7 +2,7 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # base
     if n == 0:
         return 1
@@ -12,10 +12,19 @@ def eating_cookies(n):
         return 2
     if n == 3: # three has 4 possible combinations, thus return 4
         return 4
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i: 0 for i in range(n+1)}
+        
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
+    # this return makes this algorithm O(3^n)
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 10
+    num_cookies = 1000
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
