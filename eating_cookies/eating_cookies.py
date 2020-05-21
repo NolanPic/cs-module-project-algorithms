7c0e79ed8884592820f84fbd40be039be919c071
Nolan Picini <nolanpic@gmail.com>
2020-05-21 12:21:54 -0700

    added additional comments to eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,5 +1,6 @@
 '''
 Input: an integer
+Uses a cache to yeild a runtime of O(n) instead of O(3^n)
 Returns: an integer
 '''
 def eating_cookies(n, cache=None):
@@ -12,15 +13,18 @@ def eating_cookies(n, cache=None):
         return 2
     if n == 3: # three has 4 possible combinations, thus return 4
         return 4
+    # if there is a value for n in the cache,
+    # just use it
     if cache and cache[n] > 0:
         return cache[n]
+    # there is no cache or n could not be found in it
     else:
+        # there is no cache, create it
         if not cache:
             cache = {i: 0 for i in range(n+1)}
-        
+        # set the cache to the value of the recursive calls
         cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
     
-    # this return makes this algorithm O(3^n)
     return cache[n]
 
 if __name__ == "__main__":
